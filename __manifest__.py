b1e9267dcae88710cc29fe69df89be03a5516015
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-11-07 09:24:41 -0600

    [IMP] add actives filter

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'Project Follower Custom Archive',
-    'version': '1.0',
+    'version': '1.1.0',
     'author': 'Humanytek',
     'website': 'http://humanytek.com',
     'depends': [
