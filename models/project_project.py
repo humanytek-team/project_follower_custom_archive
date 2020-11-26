42ee6107e253f57ff850bac5e8b06869a5d03551
Moisés Navarro <moisalejandro@gmail.com>
2020-11-26 12:40:07 -0600

    [MIG]

--- models/project_project.py
+++ models/project_project.py
@@ -2,13 +2,12 @@ from odoo import api, fields, models, _
 
 
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     active2 = fields.Boolean(
         default=True,
     )
 
-    @api.multi
     def toggle_active2(self):
         for r in self:
             r.active2 = not r.active2
--- models/project_task.py
+++ models/project_task.py
@@ -2,13 +2,12 @@ from odoo import api, fields, models, _
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
     active2 = fields.Boolean(
         default=True,
     )
 
-    @api.multi
     def toggle_active2(self):
         for r in self:
             r.active2 = not r.active2
