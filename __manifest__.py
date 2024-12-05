8ee22884e3859dee0101a6a8ed63ae246746582c
mbrehumanytek <matias@humanytek.com>
2024-12-05 01:58:05 +0000

    mig to 17.0s

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Project Follower Custom Archive",
-    "version": "1.2.0",
+    "version": "17.0.1.2.0",
     "author": "Humanytek",
     "website": "http://humanytek.com",
     "depends": [
